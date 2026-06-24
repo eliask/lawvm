1bc74dc6103d39c068345105f63582da5f57bb47
Elias Kunnas <elias@kunnas.com>
2026-06-24 15:28:37 +0300

    Neutralize attested oracle version alternates in bench

--- scripts/test_shard.py
+++ scripts/test_shard.py
@@ -731,6 +731,7 @@ SHARD_PATTERNS: dict[str, tuple[str, ...]] = {
         "test_bench_contract_adapters.py",
         "test_fi_bench_contract_adapter.py",
         "test_fi_bench_comparable.py",
+        "test_fi_oracle_amb_match.py",
         "test_bench_curate.py",
         "test_parse_bench.py",
         "test_parse_characterize.py",
